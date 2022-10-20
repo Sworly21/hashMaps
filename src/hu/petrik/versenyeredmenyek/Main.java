2431fff593977638a8e864e350d085eaff92b918
John Doe <johndoe@example.com>
2022-10-20 17:27:41 +0200

    Kész

--- src/hu/petrik/versenyeredmenyek/Main.java
+++ src/hu/petrik/versenyeredmenyek/Main.java
@@ -8,14 +8,34 @@ public class Main {
     private static Map<String, List<Eredmenyek>> sportagEredmenyek = new HashMap<>();
 
     public static void main(String[] args) {
-	String fajlNev = "eredmenyek.txt";
-    try {
-        beolvas(fajlNev);
-        kiiras();
-    }catch (FileNotFoundException e){
-        System.out.printf("A (z) fájl nem található", fajlNev);
+        String fajlNev = "eredmenyek.txt";
+        try {
+            beolvas(fajlNev);
+            kiiras();
+            System.out.printf("Az olimpián %d sportág van ", getSportagokSzama());
+            System.out.printf("Az olimpián %d versenyző van", getVersenyzokSzama());
+        } catch (FileNotFoundException e) {
+            System.out.printf("A (z) fájl nem található", fajlNev);
         }
     }
+
+    private static int getVersenyzokSzama(){
+        List<String> versenyzok = new ArrayList<>();
+        for (Map.Entry<String, List<Eredmenyek>> entry: sportagEredmenyek.entrySet()){
+            List<Eredmenyek> eredmeny = entry.getValue();
+            for (Eredmenyek e: eredmeny) {
+                if (!versenyzok.contains(e.getNev())){
+                    versenyzok.add(e.getNev());
+                }
+            }
+        }
+        return versenyzok.size();
+    }
+
+    private static int getSportagokSzama() {
+        return sportagEredmenyek.keySet().size();
+    }
+
 
     private static void kiiras(){
         for (Map.Entry<String, List<Eredmenyek>> entry: sportagEredmenyek.entrySet()){
